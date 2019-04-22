5720043cfaae05e55d272e3536a147fce5d79789
unknown <Sanchit@DESKTOP-CBA1IP0.mit.edu>
2019-04-22 15:54:47 -0400

    add back speech recognition

--- drumzy.py
+++ drumzy.py
@@ -80,6 +80,30 @@ def main():
     # Have the listener receive events from the controller
     controller.add_listener(listener)
 
+    #Speech Recognizer
+    r = sr.Recognizer()
+    r.pause_threshold = 0.6
+    m = sr.Microphone(sample_rate = 16000)
+
+    def callback(recognizer, audio):
+        # received audio data, now we'll recognize it using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    with m as source:
+        r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+
+    # start listening in the background (note that we don't have to do this inside a `with` statement)
+    stop_listening = r.listen_in_background(m, callback)
+    # `stop_listening` is now a function that, when called, stops background listening
+
     running = True
     while running:
         
@@ -98,6 +122,9 @@ def main():
 
     # Remove the sample listener when done
     controller.remove_listener(listener)
+
+    #stop speech when done
+    stop_listening(wait_for_stop=False)
 
 if __name__ == "__main__":
     main()
