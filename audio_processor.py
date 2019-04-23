7c703162568679a408699bee2290e2f6e0d399aa
Cesar Guerrero <guerrce@mit.edu>
2019-04-22 23:58:04 -0400

    finished audio probably

--- audio_processor.py
+++ audio_processor.py
@@ -19,38 +19,73 @@ class Note():
     def print_note(self):
         print("Pitch: %s, Start: %s" % (self.pitch, self.start_time))
 
-def main():
-    notes = []
-    MyMIDI = MIDIFile(1)
-    MyMIDI.addTempo(TRACK, TIME, BPM)
+class NoteList():
+    def __init__(self):
+        self.notes = []
+        self.midi_file = MIDIFile(1)
+        self.midi_file.addTempo(TRACK, TIME, BPM)
 
+    def add_note(self, note):
+        self.notes.append(note)
+
+    def update_midi(self):
+        for note in self.notes:
+            self.midi_file.addNote(TRACK, CHANNEL, note.pitch, TIME + note.start_time, DURATION, VOLUME)
+
+    def write_midi(self):
+        self.update_midi()
+        with open(MIDI_FILENAME, "wb") as output_file:
+            self.midi_file.writeFile(output_file)
+
+    def print_list(self):
+        for note in self.notes:
+            note.print_note()
+
+def main():
     # for testing purposes only
-    print("Press Enter to start recording")
-    sys.stdin.readline()
-    print("recording")
+
+    notes = NoteList()
     t0 = time()
-    
+    recording = False
+    counter = 0
+
+    key_map = {"a" : 60, 
+               "b" : 61, 
+               "c" : 62, 
+               "d" : 63, 
+               "e" : 64}
+
     while True:
         key = input()
         t = time() - t0
-        tt = t / (BPM/60)
-        print(tt)
+        t = t / (BPM/60)
+        print(t)
 
-        if key == "a":
-            notes.append(Note(60, tt))
-        elif key == "s":
-            notes.append(Note(62, tt))
-        elif key == "q":
+        if key == "s":    # start
+            if not recording:
+                print("starting recording round %s" % counter)
+                t0 = time()
+                recording = True
+            else:
+                print("already recording!")
+        elif key == "t":   # stop
+            if recording:
+                print("stopped recording")
+                recording = False
+                notes.update_midi()
+            else:
+                print("not recording")
+        elif key == "q":  # quit
             break
         else:
-            notes.append(Note(63, tt))
+            if key in key_map.keys():
+                notes.add_note(Note(key_map[key], t))
+            else:
+                notes.add_note(Note(65, t))
 
-    for note in notes:
-        note.print_note()
-        MyMIDI.addNote(TRACK, CHANNEL, note.pitch, TIME + note.start_time, DURATION, VOLUME)
-
-    with open(MIDI_FILENAME, "wb") as output_file:
-        MyMIDI.writeFile(output_file)
+    notes.print_list()
+    notes.write_midi()
+    
 
 
 
